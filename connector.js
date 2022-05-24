e5dac7fb94be256503a0af7a3326f6717940f532
Pedro Ferreira <pedro.ferreira@unparallel.pt>
2022-05-24 11:23:35 +0100

    added method for notifications

--- connector.js
+++ connector.js
@@ -324,6 +324,124 @@ class Connector {
         const encodedCallbackUrl = percentEncode(callbackUrl)
         return `${vc_url}/credential/issue/${encodedCredential}/callbackUrl/${encodedCallbackUrl}`;
     }
+
+    // create notification
+    // create subscription for notification
+    // mark notification as read
+    // mark notification as unread
+    // delete subscription
+    // delete notification
+    // get list notifications
+
+    /*
+    * Generic function to fetch notification data
+    */
+    async _fetchNotification(accessToken, idToken, method, service){
+        const url = this.endpoint + service;
+
+        const headers = {
+            'accept': 'application/json',
+            'Content-Type': 'application/json',
+            'access_token': accessToken,
+            'id_token': idToken
+        };
+
+        const config = {
+            method: method,
+            url: url,
+            headers: headers
+        };
+
+        Logger.debug("\nFetch URL: " + url)
+
+        try {
+            const res = await axios(config)
+            const resultData = res.data
+            if(resultData.data){
+                return resultData.data
+            }
+        } catch (e){
+            if(e.response.status === 404) {
+                const error = {
+                    statusCode: e.response.data.statusCode,
+                    statusDescription: e.response.data.statusDescription,
+                    errorMessage: JSON.parse(e.response.data.errorMessage).error
+                }
+                Logger.error(error.errorMessage.responseBody.message)
+                return []
+            }
+            else {
+                throw new FetchError(e)
+            }
+        }
+    }
+
+    async getAllNotifications(accessToken, idToken){
+         return await this._fetchNotification(accessToken, idToken, "GET", `/SdkRefImpl/api/sdk-ri/notification`);
+    }
+
+    async getUserNotifications(accessToken, idToken, user){
+        return await this._fetchNotification(accessToken, idToken, "GET", `/SdkRefImpl/api/sdk-ri/notification/user/${user}`);
+    }
+
+    async getUserUnreadNotifications(accessToken, idToken, user){
+        return await this._fetchNotification(accessToken, idToken, "GET", `/SdkRefImpl/api/sdk-ri/notification/user/${user}/unread`);
+    }
+
+    async markNotificationsAsRead(accessToken, idToken, notificationId){
+         return await this._fetchNotification(accessToken, idToken, "PATCH", `/SdkRefImpl/api/sdk-ri/notification/${notificationId}/read`);
+    }
+
+    async markNotificationsAsUnread(accessToken, idToken, notificationId){
+        return await this._fetchNotification(accessToken, idToken, "PATCH", `/SdkRefImpl/api/sdk-ri/notification/${notificationId}/unread`);
+    }
+
+    async getNotification(accessToken, idToken, notificationId){
+        return await this._fetchNotification(accessToken, idToken, "GET", `/SdkRefImpl/api/sdk-ri/notification/${notificationId}`);
+    }
+
+    async deleteNotification(accessToken, idToken, notificationId){
+        return await this._fetchNotification(accessToken, idToken, "DELETE", `/SdkRefImpl/api/sdk-ri/notification/${notificationId}`);
+    }
+
+    async createNotification(accessToken, idToken, origin, receiver_id, type, message, status){
+        const url = `${this.endpoint}/SdkRefImpl/api/sdk-ri/notification`;
+
+        const headers = {
+            'accept': 'application/json',
+            'Content-Type': 'application/json',
+            'access_token': accessToken,
+            'id_token': idToken
+        }
+
+        const data = {
+            "origin": origin,
+            "predefined": true,
+            "type": type,
+            "receiver_id": receiver_id,
+            "message": message,
+            "status": status
+        }
+
+        const config = {
+            method: 'post',
+            url: url,
+            headers: headers,
+            data : JSON.stringify(data)
+        }
+
+        Logger.debug("\nFetch URL: " + url)
+
+        try {
+            const res = await axios(config)
+            const resultData = res.data
+            if(resultData.data){
+                return resultData.data
+            }
+        } catch (e){
+            throw new FetchError(e)
+        }
+    }
 }
 
 exports.Connector = Connector
