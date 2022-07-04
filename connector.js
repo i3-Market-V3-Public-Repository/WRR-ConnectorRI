517ad91b0b65215cf461f21c0841b4e243a9d6b9
Pedro Ferreira <pedro.ferreira@unparallel.pt>
2022-07-04 12:15:59 +0100

    add contract methods

--- connector.js
+++ connector.js
@@ -567,26 +567,59 @@ class Connector {
         }
     }
 
+    async processNotificationWebhook(accessToken, idToken){
+        const url = 'https://webhook.site/token/061f4585-3cc6-416f-ae4b-0c56e7b33cba/requests'
+
+        const config = {
+            method: 'GET',
+            url: url
+        }
+
+        Logger.debug("\nFetch URL: " + url)
+
+        try {
+            const res = await axios(config)
+            const resData = res.data.data
+            const postData = resData.filter(el => el.method === 'POST')
+            const lastItem = postData[postData.length - 1]
+            const content = JSON.parse(lastItem.content)
+
+            const message = {
+                msg: `Agreement pending for offering ${content.data.contractualParameters.DataOfferingDescription.dataOfferingId}`,
+                template: content.data.contractualParameters
+            }
+            const type = `${content.action.toLowerCase()}.${content.status.toLowerCase()}`
+
+            return await this.createNotification(accessToken, idToken, 'i3m', content.data.providerDID, type, message, 'OK');
+        } catch (e){
+            throw new FetchError(e)
+        }
+    }
+
     /*
     *
     * CONTRACTS
     *
     */
-    async getContractTemplate(accessToken, idToken, offeringId){
-        const url = `${this.endpoint}/SdkRefImpl/api/sdk-ri/contract/get-contract-template/${offeringId}`;
+    async _fetchContract(accessToken, idToken, method, service, data = undefined, authorization = undefined){
+        const url = this.endpoint + service;
 
-        const headers = {
+        let headers = {
             'accept': 'application/json',
             'Content-Type': 'application/json',
             'access_token': accessToken,
             'id_token': idToken
         };
+        if(authorization)
+            headers['Authorization'] = authorization;
 
-        const config = {
-            method: 'GET',
+        let config = {
+            method: method,
             url: url,
             headers: headers
         };
+        if(data)
+            config.data = JSON.stringify(data)
 
         Logger.debug("\nFetch URL: " + url);
 
@@ -605,42 +638,36 @@ class Connector {
         }
     }
 
-    async createDataPurchase(accessToken, idToken, authorization, originMarketId, data){
-        const url = `${this.endpoint}/SdkRefImpl/api/sdk-ri/contract/create-data-purchase?origin_market_id=${originMarketId}`;
+    async getContractTemplate(accessToken, idToken, offeringId){
+        return await this._fetchContract(accessToken, idToken, 'GET', `/SdkRefImpl/api/sdk-ri/contract/get-contract-template/${offeringId}`);
+    }
 
-        const headers = {
-            'accept': 'application/json',
-            'Content-Type': 'application/json',
-            'access_token': accessToken,
-            'id_token': idToken,
-            'Authorization': authorization
-        }
+    async createDataPurchase(accessToken, idToken, originMarketId, consumerDid, authorization, data){
+        return await this._fetchContract(accessToken, idToken, 'POST', `/SdkRefImpl/api/sdk-ri/contract/create-data-purchase?origin_market_id=${originMarketId}&consumer_did=${consumerDid}`, data, authorization)
+    }
 
-        const config = {
-            method: 'post',
-            url: url,
-            headers: headers,
-            data : JSON.stringify(data)
-        }
+    async createAgreementRawTransaction(accessToken, idToken, senderAddress, data){
+        return await this._fetchContract(accessToken, idToken, 'POST', `/SdkRefImpl/api/sdk-ri/contract/create_agreement_raw_transaction/${senderAddress}`, data);
+    }
 
-        Logger.debug("\nFetch URL: " + url)
+    async deploySignedTransaction(accessToken, idToken, data){
+        return await this._fetchContract(accessToken, idToken, 'POST',`/SdkRefImpl/api/sdk-ri/contract/deploy_signed_transaction`, data);
+    }
 
-        try {
-            const res = await axios(config)
-            const resultData = res.data
-            if(resultData.data){
-                return resultData.data
-            }
-        } catch (e){
-            if(e.response.status === 401) {
-                const error = {
-                    statusCode: e.response.data.statusCode,
-                    message: e.response.data.statusDescription
-                }
-                throw new FetchError(error)
-            }
-            throw new FetchError(e)
-        }
+    async getAgreementState(accessToken, idToken, agreementId){
+        return await this._fetchContract(accessToken, idToken, 'GET',`/SdkRefImpl/api/sdk-ri/contract/state/${agreementId}`);
+    }
+
+    async getAgreement(accessToken, idToken, agreementId){
+        return await this._fetchContract(accessToken, idToken, 'GET',`/SdkRefImpl/api/sdk-ri/contract/get_agreement/${agreementId}`);
+    }
+
+    async getAgreementsByConsumer(accessToken, idToken, consumerId){
+        return await this._fetchContract(accessToken, idToken, 'GET',`/SdkRefImpl/api/sdk-ri/contract/check_agreements_by_consumer/${consumerId}`);
+    }
+
+    async getAgreementsByProvider(accessToken, idToken, providerId){
+        return await this._fetchContract(accessToken, idToken, 'GET',`/SdkRefImpl/api/sdk-ri/contract/check_agreements_by_provider/${providerId}`);
     }
 }
 
